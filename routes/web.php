fbedd4a9a06b001739242043ba9a62996a7a8093
berlanga2008 <berlanga2008@gmail.com>
2021-11-25 14:15:03 +0100

    clears cachesses

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,42 @@ Route::get('dashboard', [AuthController::class, 'dashboard']);
 Route::get('dashboard/{id}', [AuthController::class, 'dashboardid']);
 Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
+//Clear Cache facade value:
+Route::get('/clear-cache', function () {
+    $exitCode = Artisan::call('cache:clear');
+    return '<h1>Cache facade value cleared</h1>';
+});
+
+//Reoptimized class loader:
+Route::get('/optimize', function () {
+    $exitCode = Artisan::call('optimize');
+    return '<h1>Reoptimized class loader</h1>';
+});
+
+//Route cache:
+Route::get('/route-cache', function () {
+    $exitCode = Artisan::call('route:cache');
+    return '<h1>Routes cached</h1>';
+});
+
+//Clear Route cache:
+Route::get('/route-clear', function () {
+    $exitCode = Artisan::call('route:clear');
+    return '<h1>Route cache cleared</h1>';
+});
+
+//Clear View cache:
+Route::get('/view-clear', function () {
+    $exitCode = Artisan::call('view:clear');
+    return '<h1>View cache cleared</h1>';
+});
+
+//Clear Config cache:
+Route::get('/config-cache', function () {
+    $exitCode = Artisan::call('config:cache');
+    return '<h1>Clear Config cleared</h1>';
+});
+
 
 /*Route::get('registration', [AuthController::class, 'registration'])->name('register');*/
 /*Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post');*/
